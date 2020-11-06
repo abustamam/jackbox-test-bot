c0686d926486cbc79594d0f743c0c8a67b933ba9
Rasheed <rasheed.bustamam@gmail.com>
2020-11-06 10:22:19 -0800

    initial commit

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,44 @@
 import { createServer } from 'http'
-import { parse } from 'url'
 import next from 'next'
+import Discord from 'discord.js';
 
-const port = parseInt(process.env.PORT || '3000', 10)
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+const roasts = [
+  'sus',
+  'an imposter',
+  'Jack',
+  'Kody',
+  'a piece of :poop:'
+];
+
+client.on('message', message => {
+  if (message.content.startsWith('!roast')) {
+    if ((message.channel as Discord.TextChannel).id !== '769393944487788554') {
+      message.reply(`No roasting outside the <#769393944487788554> you meanie`);
+    } else if (message.mentions.users.size) {
+      const taggedUser = message.mentions.users.first();
+      const user = message.author;
+      const randomRoast = roasts[Math.floor(Math.random() * roasts.length)];
+      message.channel.send(`<@${user?.id}> says <@${taggedUser?.id}> is ${randomRoast}`);
+    }
+  }
+});
+
+const port = parseInt(process.env.PORT || '3001', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+
+console.log(process.env.TOKEN);
+
+client.login(process.env.TOKEN);
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    const { pathname, query } = parsedUrl
-
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
+  createServer(() => {
   }).listen(port)
 
   // tslint:disable-next-line:no-console
